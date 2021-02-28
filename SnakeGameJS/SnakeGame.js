d90576ccd0f956b720b7c8dac39ecf792f3118e5
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-28 18:10:44 -0300

    finishing snake size and color

--- SnakeGameJS/SnakeGame.js
+++ SnakeGameJS/SnakeGame.js
@@ -3,12 +3,32 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 //32 pixel cada quadrado em que o jogo sera montado
 let box = 32;
+/*A cobra do jogo sera feita com um array de coordenadas, a cobra irá andar da seguinte forma:
+"adiciona um dado e retira o ultimo"*/
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
 
 //function starting canvas
-function criarBG() {
-  //fillStyle trabalha com o canvas,contexto
-  context.fillStyle = "lightgreen";
+function createBG() {
+  //fillStyle trabalha com o canvas,construindo o background da aplicacao
+  context.fillStyle = "#222733";
   //fillRect desenha o retangulo onde ira acontecer o jogo
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
-criarBG();
+
+function createSnake() {
+  /*O for irá percorrer todo o tamanho do array e vai incrementar, quando ele for incrementado 
+  ele ira pintar o corpo da snake de verde e ira setar o tamanho dela corretamente*/
+  for(i=0;i<snake.length; i++){
+      //cor snake
+      context.fillStyle = "#7FFFD4";
+      //tamanho snake
+      context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+createBG();
+createSnake();
