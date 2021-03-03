a3ab3122bf0a19a841552fabb9ddbbb35defa9c6
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-03 00:00:56 -0300

    Challenge delete

--- ChallengesBasic/5-Challenge.js
+++
@@ -1,9 +0,0 @@
-/*Você deve calcular o consumo médio de um automóvel onde será informada 
-a distância total percorrida (em Km) e o total de combustível consumido (em litros). */
-
-
-
-let a = parseInt(gets());
-let b = parseFloat(gets());
-let total = parseFloat(a / b).toFixed(3);
-console.log(total + " km/l");
