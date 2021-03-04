73ad87a93ec86fbb6e7746e7e930489f902f985d
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-04 17:22:48 -0300

    ThisJS

---
+++ JavaScriptES6/3-FunctionsAndOperators/3-This.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(){
+        this.name="Arthur";
+    }
+}
+
+
+
+function callPerson(){
+   let person = new Person();
+   console.log(person.name);
+}
+
+callPerson();
+
