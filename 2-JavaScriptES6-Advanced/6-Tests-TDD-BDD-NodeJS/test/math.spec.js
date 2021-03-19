0c602e1d91011196ea55afbfa25f4f71554ac68c
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-19 18:24:48 -0300

    updating Tests with mocha-chai-sinon - math.spec.js

--- 2-JavaScriptES6-Advanced/6-Tests-TDD-BDD-NodeJS/test/math.spec.js
+++ 2-JavaScriptES6-Advanced/6-Tests-TDD-BDD-NodeJS/test/math.spec.js
@@ -6,6 +6,6 @@ describe('Math class', function(){
     it('Sum two numbers', function(){
         const math = new Math();
 
-        assert.equal(math.sum(5,5), 10);
+        assert.equal(math.sum(5,5), 14);
     });
 });
