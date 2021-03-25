eaff99f90067f765fece94f31d1d026bdda77aaa
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-25 18:56:44 -0300

    Starting React study

---
+++ ReactJS/React-Basic/reactbasic/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+const element = ''
+const rootElement = document.getElementById("root")
+ReactDOM.render(element, rootElement)
