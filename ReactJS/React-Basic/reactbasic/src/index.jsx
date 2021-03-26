bf6ed49aefa1df92958405d669a085a0e73f0832
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-25 21:09:52 -0300

    delete React study

--- ReactJS/React-Basic/reactbasic/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./style.css"
-
-const element = ''
-const rootElement = document.getElementById("root")
-ReactDOM.render(element, rootElement)
