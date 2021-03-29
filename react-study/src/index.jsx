4b61d26d74828510e057fe766240f68a31e865b0
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-29 16:30:20 -0300

    creating components extends reactComponent - study props and setstate

--- react-study/src/index.jsx
+++ react-study/src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Button from './components/Button';
-import Post from './components/Post'
 
 ReactDOM.render(
   <React.StrictMode>
