52b86768c4ce22143c595a5c1dff13d3eae8beed
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-29 00:57:39 -0300

    Starting study ReactJS

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,7 +1,19 @@
-function App() {
+/*function App() {
   return (
     <h1>Hello World</h1>
   );
+}*/
+const buttonA = <button>Primeiro button</button>
+const buttonB = <button>Segundo button</button>
+
+const App = ()=>{
+  return (
+    <div>
+      <p>Pentrei aqui</p>
+      {buttonA}
+      {buttonB}
+    </div>
+  )
 }
 
 export default App;
