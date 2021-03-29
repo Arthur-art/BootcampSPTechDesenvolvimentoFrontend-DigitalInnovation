0e3c02edda63bd9fb4d1f71157c125b8ea7465bf
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-29 14:03:14 -0300

    creating components and adding dom

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <p>Hello World</p>
+      
     </div>
   );
 };
--- react-study/src/index.jsx
+++ react-study/src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Button from './components/Button';
+import ListKeys from './components/ListsKeys';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Button />
+    <ListKeys />
   </React.StrictMode>,
   document.getElementById('root')
 );
