f984e4697b2f603ee2f99a5ff6952ec3ce65100d
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-29 22:33:10 -0300

    adicionando events onclick

--- react-study/src/componentscodecademy/Oneclick.jsx
+++ react-study/src/componentscodecademy/Oneclick.jsx
@@ -3,10 +3,9 @@ export default class Oneclick extends React.Component{
     render(){
         const makeDog =(e)=> {
             e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
-            e.target.setAttribute('alt', 'Dog')
         };
         const cat = 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg';
-        const linkCat = <img onClick={makeDog} src={cat} alt="Cat"/>;
+        let linkCat = <img onClick={makeDog} src={cat} alt="Cat"/>;
         return (
             <div>
                 {linkCat}
