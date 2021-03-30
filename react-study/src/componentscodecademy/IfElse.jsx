f53df8b2b3ce4590c78f5dab73e54f166b3cc2d0
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-29 22:56:29 -0300

    adicionando if else

--- react-study/src/componentscodecademy/IfElse.jsx
+++ react-study/src/componentscodecademy/IfElse.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
+import React from "react";
 
-export default class IfElse extends React.Component{
-    render(){
+export default class IfElse extends React.Component {
+  render() {
+    const cointToss = () => {
+      return Math.random() < 0.5 ? "heads" : "tails";
+    };
 
-        const cointToss = ()=>{
-            return Math.random() < 0.5 ? 'heads' : 'tails';
-        };
+    const pics = {
+      kitty:
+        "https://content.codecademy.com/courses/React/react_photo-kitty.jpg",
+      doggy:
+        "https://content.codecademy.com/courses/React/react_photo-puppy.jpeg",
+    };
+    let img;
 
-        const pics = {
-            kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
-            doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
-          };
-          let img;
-
-          if(cointToss === 'tails'){
-              img = (
-                 <img src={pics.kitty} alt=""/>
-              );
-          }else{
-              img = (
-                  <img src={pics.doggy} alt=""/>
-              )
-          }
-
-
-      return (
-          <div>
-              {img}
-          </div>
-      )  
+    if (cointToss === "tails") {
+      img = <img src={pics.kitty} alt="" />;
+    } else {
+      img = <img src={pics.doggy} alt="" />;
     }
+
+    return <div>{img}</div>;
+  }
 }
