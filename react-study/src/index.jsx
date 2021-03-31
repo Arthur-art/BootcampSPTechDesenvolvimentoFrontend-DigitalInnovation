a567b40fe39a74b664341548a5831fe1d48f1bf7
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-30 22:16:53 -0300

    refatorando index react

--- react-study/src/index.jsx
+++ react-study/src/index.jsx
@@ -1,11 +1,18 @@
+/*import React from 'react'cria um objeto JavaScript. Este objeto contém propriedades que são 
+necessárias para fazer o React funcionar, como React.createElement()e React.Component. */
 import React from 'react'
+/*import ReactDOM from 'react-dom'cria outro objeto JavaScript. 
+Este objeto contém métodos que ajudam o React a interagir com o DOM, como ReactDOM.render(). */
 import ReactDOM from 'react-dom'
-import Map from './componentscodecademy/Map'
-import Keys from './componentscodecademy/Keys'
 
+const Div = React.createElement("h1", null, "React - codecademy");
+const attribut = (e)=> {
+  e.target.setAttribute("style", "font-size:3rem;")
+}
+const DivTitle = <div onMouseOver={attribut}>{Div}</div>
 ReactDOM.render(
   <React.StrictMode>
-    <Keys/>
+    <div>{DivTitle}</div>
   </React.StrictMode>,
   document.getElementById('container')
 );
