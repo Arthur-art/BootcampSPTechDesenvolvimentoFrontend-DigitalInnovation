310b1a8388e87423476a41df220a08cacc5d8956
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-05 19:06:11 -0300

    delete react-project

--- 3-ReactJS-Intermediary/src/index.jsx
+++ 3-ReactJS-Intermediary/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CssComponent from './components/CssComponent'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssComponent />
+
   </React.StrictMode>,
   document.getElementById('container')
 );
 
+ 
